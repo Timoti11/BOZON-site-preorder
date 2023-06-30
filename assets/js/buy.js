05cc74fc1b09060b561ee91c70fbcbc4746cb31c
Timoti11 <timastanskov@mail.ru>
2023-06-30 15:01:20 +0400

    Third
    Плашки крутяцца нахооой

---
+++ assets/js/buy.js
@@ -0,0 +1,40 @@
+$('#buy-1').click(function() {
+    $('#layer-1').css({
+        'visibility': 'hidden',
+        'display': 'none'
+    });
+    $('#layer-2').css({
+        'visibility': 'visible',
+        'display': 'block'
+    });
+});
+$('#buy-1-back').click(function() {
+    $('#layer-1').css({
+        'visibility': 'visible',
+        'display': 'block'
+    });
+    $('#layer-2').css({
+        'visibility': 'hidden',
+        'display': 'none'
+    });
+});
+$('#buy-2').click(function() {
+    $('#layer-1_2').css({
+        'visibility': 'hidden',
+        'display': 'none'
+    });
+    $('#layer-2_2').css({
+        'visibility': 'visible',
+        'display': 'block'
+    });
+});
+$('#buy-2-back').click(function() {
+    $('#layer-1_2').css({
+        'visibility': 'visible',
+        'display': 'block'
+    });
+    $('#layer-2_2').css({
+        'visibility': 'hidden',
+        'display': 'none'
+    });
+});
