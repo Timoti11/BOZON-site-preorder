58dbe16ccedd9a71fc4494c14abea8810e7914f8
Timoti11 <timastanskov@mail.ru>
2023-07-01 10:33:13 +0400

    Оферта

--- assets/js/buy.js
+++ assets/js/buy.js
@@ -44,9 +44,24 @@ $('#buy-2-back').click(function() {
 /* Ошибка */
 
 var blockHidden = document.querySelector('.alert');
+var oferta = document.querySelector('.oferta');
+var blur = document.querySelector('#blur');
 
 $('#rules-1').click(function() {
-    console.log('1')
     blockHidden.classList.add('alert-on');
     setTimeout(() => blockHidden.classList.add('alert-off'), 5000);
+});
+
+$('#oferta-1').click(function() {
+    $('.oferta').css({
+        'visibility': 'visible'
+    });
+    blur.classList.add('blur');
+});
+
+$('#oferta-close').click(function() {
+    $('.oferta').css({
+        'visibility': 'hidden'
+    });
+    blur.classList.remove('blur');
 });
